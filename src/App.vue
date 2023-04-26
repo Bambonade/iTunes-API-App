a179118f5ed043928c1ff29672ff8b537eb33e4b
Brandon Nelson <machinecar998@gmail.com>
2023-04-26 12:59:31 -0500

    tried to fix

--- src/App.vue
+++ src/App.vue
@@ -2,11 +2,6 @@
   <nav class="navbar bg-dark">
     <span class="navbar-brand text-light "><i class="fas fa-shopping-bag ms-4 me-3"></i> CHECK IT OUT!</span>
   </nav>
-    <form id="search">
-        <label for="searchTerm">Search</label>
-        <input type="text" id="searchTerm">
-        <div id="results"></div>
-    </form>
   <div class="container-fluid mt-3">
     <library-item-list></library-item-list>
   </div>
--- src/components/LibraryItemList.vue
+++ src/components/LibraryItemList.vue
@@ -1,4 +1,9 @@
 <template>
+    <form id="search">
+        <label for="searchTerm">Search</label>
+        <input type="text" id="searchTerm">
+        <div id="results"></div>
+    </form>
     <div class="row row-cols-1 row-cols-sm-2 row-cols-lg-3 row-cols-xxl-4 g-3">
         <div class="col" v-for="item in library" :key="item.name">
             <library-item-card :item="item"/>
@@ -10,7 +15,9 @@
 import LibraryItemCard from "@/components/LibraryItemCard.vue";
 import LibraryCollection from "@/models/LibraryCollection";
 import {Book, Movie} from "@/models/Media";
+//import axios from 'axios';
 
+//let searchTerm = document.getElementById('#searchTerm').value;
 export default {
     name: "LibraryItemList",
     components: {LibraryItemCard},
@@ -24,9 +31,16 @@ export default {
                 .addItem(new Book('1984', 237))
                 .addItem(new Movie('Ready Player 1', 122))
                 .addItem(new Movie('Avengers: End Game', 189))
+            // response: await axios.get('https://itunes.apple.com/search', {
+            //     params: {
+            //         term: searchTerm,
+            //         filter: 'full',
+            //         limit: 36,
+            //     }
+            // }),
         }
     },
-}
+};
 </script>
 
 <style scoped>
