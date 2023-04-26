2e3fbf60a34014db822782426b64795d1ea3a22d
Brandon Nelson <machinecar998@gmail.com>
2023-04-25 20:12:40 -0500

    created movie model

---
+++ src/components/SongDetails.vue
@@ -0,0 +1,22 @@
+<template>
+<div class="song">
+    <h3 class="card-title"></h3>
+    <img src="$({{item.thumbnail}})">
+    <p class="card-text"></p>
+</div>
+</template>
+
+<script>
+import Song from '@/models/Song'
+
+export default {
+    name: 'SongDetails.vue',
+    props:{
+        item: {type:Song}
+    }
+}
+</script>
+
+<style scoped>
+
+</style>
---
+++ src/models/Song.js
@@ -0,0 +1,44 @@
+export default class Song {
+    static type ='Song';
+
+    get type(){
+        return Song.type;
+    }
+
+    //postman list
+    wrapperType = ' ';
+    kind= "";
+    artistId= 0;
+    collectionId= 0;
+    trackId= 0;
+    artistName= "";
+    collectionName= "";
+    trackName= "";
+    collectionCensoredName= "";
+    trackCensoredName= "";
+    artistViewUrl= "";
+    collectionViewUrl= "";
+    trackViewUrl= "";
+    previewUrl= "";
+    artworkUrl30= "";
+    artworkUrl60= "";
+    artworkUrl100= "";
+    collectionPrice= 0;
+    trackPrice= 0;
+    releaseDate= "";
+    collectionExplicitness= "";
+    trackExplicitness= "";
+    discCount= 0;
+    discNumber= 0;
+    trackCount= 0;
+    trackNumber= 0;
+    trackTimeMillis= 0;
+    country= "";
+    currency= "";
+    primaryGenreName= "";
+    isStreamable=Boolean;
+
+    get thumbnail(){
+        return this.artworkUrl100?.replace('&edge=curl', '')
+    }
+}
